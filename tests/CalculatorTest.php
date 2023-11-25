bfc2d58a10b32b4f85e07c62f1a7350e9fcdb2d9
salma <salmababa484@gmail.com>
2023-11-26 00:10:14 +0100

    mo4

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -8,7 +8,7 @@ class CalculatorTest extends PHPUnit\Framework\TestCase
         $calculator = new App\Calculator();
 
         // Teste si l'addition fonctionne correctement
-        $this->assertEquals(5, $calculator->add(3, 2));
+        $this->assertEquals(7, $calculator->add(3, 4));
 
         // Teste si l'addition fonctionne avec des nombres négatifs
         $this->assertEquals(-2, $calculator->add(2, -4));
